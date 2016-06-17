7d08d23bdf1a8acf520125c4583f82d98df9c157
Shahriar <shahriarkamal.dsi@gmail.com>
2016-06-17 16:51:25 +0600

    not tested (advance huristicanAnalysis  added  ) version -> 34

--- java-bot/src/bot/MiniMaxAlgorith.java
+++ java-bot/src/bot/MiniMaxAlgorith.java
@@ -30,7 +30,8 @@ public Integer miniMax(Field game,int depth,int botID,int alpha,int bita) {
         	
         }
 		List availabelMoves = game.getAvailableMoves() ; 
-		if(depth >this._depth) return huristicanAnalysis(game,botID); 
+		if(depth >this._depth && this._depth == 3 ) return huristicanAnalysis(game,botID);  
+		else if(depth >this._depth) return advanceHuristicAnalysis(game, oponentBotID, opID)  ;
 		if(availabelMoves.size()>9 ) {
 			if(botID == BotParser.mBotId) return bestmoveByGreedyAlgo(game,botID,oponentBotID,depth) +10;  
 			return bestmoveByGreedyAlgo(game,botID,oponentBotID,depth) -10;
